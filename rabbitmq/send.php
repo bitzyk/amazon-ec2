f74453c9113c84c7a9429b43c1c4453c1205acae
cbitoi <cbitoi@ebay.com>
2015-06-22 23:46:10 +0100

    Rabbitmq

--- rabbitmq/send.php
+++ rabbitmq/send.php
@@ -5,7 +5,7 @@
  * Date: 22/06/15
  * Time: 23:34
  */
-
+phpinfo(); exit;
 require_once __DIR__ . '/vendor/autoload.php';
 
 use PhpAmqpLib\Connection\AMQPConnection;
@@ -14,4 +14,4 @@ use PhpAmqpLib\Message\AMQPMessage;
 $connection = new AMQPConnection('localhost', 5672, 'guest', 'guest');
 $channel = $connection->channel();
 
-print_r($channel);
+$channel->queue_declare('bitzy first queue in rabbitmq', false, false, false, false);
