4ef5a15a5a6d36c59e0e31a556fc084824a20ee2
cbitoi <cbitoi@ebay.com>
2015-06-22 23:37:46 +0100

    Rabbitmq

---
+++ rabbitmq/send.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cbitoi
+ * Date: 22/06/15
+ * Time: 23:34
+ */
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+use PhpAmqpLib\Connection\AMQPConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+$connection = new AMQPConnection('localhost', 5672, 'guest', 'guest');
+$channel = $connection->channel();
+
+print_r($channel);
